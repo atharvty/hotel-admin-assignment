b7f072647df65f1aa872c71ee82856a9ab59aaeb
atharvty <73381314+atharvty@users.noreply.github.com>
2023-03-11 21:16:16 +0530

    changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-black">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
-        <a href="https://flowbite.com/" class="flex items-center">
+        <a href="http://localhost:3000/" class="flex items-center">
           {/* <img
             src="https://flowbite.com/docs/images/logo.svg"
             class="h-6 mr-3 sm:h-9"
